44803af1b1844dd9385364716a6723a5280b7a32
James Chuang <chuang.ja@gmail.com>
2018-06-27 18:37:44 -0400

    Update molecular barcode extraction to use subprocess pigz instead of python gzip library for speed.

--- scripts/extract_molecular_barcode.py
+++ scripts/extract_molecular_barcode.py
@@ -2,7 +2,7 @@
 
 """
 Date : April 24th, 2014
-Last updated: June 26th, 2018
+Last updated: June 27th, 2018
 
 extract molecular barcode from reads and write out barcode and ligation counts
 
@@ -10,33 +10,35 @@ use: python extract_molecular_barcode.py in_fastq out_fastq out_barcode out_liga
 """
 
 def main():
-    import sys, itertools, gzip
+    import sys, itertools, subprocess
     from collections import Counter
 
     nucleotides = 'ACTGN'
     barcodes_dict = Counter({"".join(nts) : 0 for nts in itertools.product(nucleotides, repeat=6)})
     ligations_dict = Counter({"".join(nts) : 0 for nts in itertools.product(nucleotides, repeat=6)})
 
-    with gzip.open(sys.argv[1], 'rt') as in_fastq, gzip.open(sys.argv[2], 'wt') as out_fastq, open(sys.argv[3], 'w') as out_barcode, open(sys.argv[4], 'w') as out_ligation:
-
-        header = in_fastq.readline().rstrip()
+    with open(sys.argv[2], 'w') as out_fastq_file:
+        in_fastq= subprocess.Popen(['pigz', '-cdfq', sys.argv[1]], stdout=subprocess.PIPE, encoding='utf-8')
+        out_fastq= subprocess.Popen(['pigz', '-fq', '-'], stdin=subprocess.PIPE, stdout=out_fastq_file, encoding='utf-8')
+        header = in_fastq.stdout.readline().rstrip()
         while header != '':
-            full_seq = in_fastq.readline()
-            plus_line = in_fastq.readline()
-            qual_scores = in_fastq.readline()
+            full_seq = in_fastq.stdout.readline()
+            plus_line = in_fastq.stdout.readline()
+            qual_scores = in_fastq.stdout.readline()
             barcode = full_seq[0:6]
             ligation = full_seq[3:9]
             trimmed_seq = full_seq[6:]
-            out_fastq.write(header.split(" ")[0]+'_MolecularBarcode:'+barcode+' '+header.split(" ")[1]+'\n')
-            out_fastq.write(trimmed_seq)
-            out_fastq.write(plus_line)
-            out_fastq.write(qual_scores[6:])
-            header = in_fastq.readline().rstrip()
+            out_fastq.stdin.write(header.split(" ")[0]+'_MolecularBarcode:'+barcode+' '+header.split(" ")[1]+'\n')
+            out_fastq.stdin.write(trimmed_seq)
+            out_fastq.stdin.write(plus_line)
+            out_fastq.stdin.write(qual_scores[6:])
+            header = in_fastq.stdout.readline().rstrip()
 
             barcodes_dict.update([barcode])
             if len(trimmed_seq) >= 4 :
                 ligations_dict.update([ligation])
 
+    with open(sys.argv[3], 'w') as out_barcode, open(sys.argv[4], 'w') as out_ligation:
         out_barcode.write("barcode\tcount\n")
         for barcode, count in barcodes_dict.items():
             out_barcode.write(f"{barcode}\t{count}\n")
